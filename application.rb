deb9c8baa162d34dc2f9de1f52dedadc913eb462
HARRY BHATT <bob@brahmingate.co.uk>
2019-03-10 13:56:03 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World my name is Harry"
+    resp.write "Hello, my name is Harry"
     resp.finish
   end
 
